8a5f8d4555a790ccab498c6b6fda1a563eb6d869
Mukul1320 <51812402+Mukul1320@users.noreply.github.com>
2020-07-12 09:31:55 +0530

    Add files via upload

---
+++ DICE_SIMULATOR.py
@@ -0,0 +1,84 @@
+import random
+x="y" or "y"
+while x=="Y" or x=="y":
+    number=random.randint(1,6)
+    print(number)
+    if number==1:
+    
+       print("*----------------*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|      0       |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*----------------*")
+    if number==2:
+    
+       print("*----------------*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|    0    0    |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*----------------*")
+    if number==3:
+    
+       print("*----------------*")
+       print("*|              |*")
+       print("*| 0            |*")
+       print("*|              |*")
+       print("*|      0       |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|            0 |*")
+       print("*|              |*")
+       print("*----------------*")
+    if number==4:
+     
+       print("*----------------*")
+       print("*|              |*")
+       print("*|  0        0  |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|  0        0  |*")
+       print("*|              |*")
+       print("*----------------*")
+    if number==5:
+    
+       print("*----------------*")
+       print("*|              |*")
+       print("*| 0          0 |*")
+       print("*|              |*")
+       print("*|      0       |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*| 0          0 |*")
+       print("*|              |*")
+       print("*----------------*")
+    if number==6:
+    
+       print("*----------------*")
+       print("*|  0         0 |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|  0         0 |*")
+       print("*|              |*")
+       print("*|              |*")
+       print("*|  0         0 |*")
+       print("*|              |*")
+       print("*----------------*")
+          
+
+
+    x=input("Y or N ")
+          
+          
+
